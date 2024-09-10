99d824456ba721101620620f7579d4a402d8330f
analuisa <analuisaaugustob@gmail.com>
2024-09-10 16:47:35 -0300

    Rodando teste via SauceLab.

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,11 +1,28 @@
 export const config = {
-    runner: 'local',
-    port: 4723,
+    // runner: 'local',
+    // port: 4723,
+    user: 'oauth-analuisaaugustob-7ca2a',
+    key: '68fc44c9-7450-4d3b-a445-ac6cc9f561e1',
+    hostname: 'ondemand.us-west-1.saucelabs.com',
+    port: 443,
+    baseUrl: 'wd/hub',
     specs: [
         './test/specs/**/*.js'
     ],
     maxInstances: 1,
-    // capabilities: [{
+    capabilities: [{
+        platformName: 'Android',
+        'appium:app': 'storage:filename=ebacshop.aab',
+        'appium:deviceName': 'Android GoogleAPI Emulator',
+        'appium:platformVersion': '10',
+        "appium:disableIdLocatorAutocompletion": true,
+        'appium:automationName': 'UiAutomator2',
+        'sauce:options': {
+            build: 'appium-build-teste-ebacshop',
+            name: 'Ebac Shop Teste',
+            deviceOrientation: 'PORTRAIT',
+            appiumVersion: '2.11.0',
+        }
     //     platformName: 'Android',
     //     'appium:deviceName': 'ebac',
     //     'appium:platformVersion': '9.0',
@@ -13,7 +30,7 @@ export const config = {
     //     'appium:app': `${process.cwd()}/app/ebacshop.apks`,
     //     'appium:appWaitActivity': '.MainActivity',
     //     'appium:DisableIdLocatorAutocompletion': true
-    // }],
+    }],
     logLevel: 'info',
     bail: 0,
     waitforTimeout: 10000,
