0f5b5a8b670476402d3538f60fc32198f038c6b6
analuisa <analuisaaugustob@gmail.com>
2024-09-10 11:03:39 -0300

    :white_check_mark:test: Refatorando Login teste.

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -1,8 +1,14 @@
 import { expect, driver } from '@wdio/globals'
+import homePage from '../pageobjects/home.page'
+import loginPage from '../pageobjects/login.page'
+import profilePage from '../pageobjects/profile.page'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await driver.pause(10000)
+        await homePage.openMenu('profile')
+        await loginPage.login('ana@luisa.barbosa.com.br', 'anaLuisa97!')
+        await homePage.openMenu('profile')
+        expect((await profilePage.profileName('Barbosa Ana Luisa')).isDisplayed()).toBeTruthy()     
     })
 })
 
