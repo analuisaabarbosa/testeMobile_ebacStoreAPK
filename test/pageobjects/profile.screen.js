3885fd75c2932ae920bc3fd47a59dc60cbaa0c30
analuisa <analuisaaugustob@gmail.com>
2024-09-12 13:28:12 -0300

    Relatório.

--- test/pageobjects/profile.screen.js
+++
@@ -1,9 +0,0 @@
-import { $ } from '@wdio/globals'
-
-class ProfileScreen {
-    async profileName(name) {
-        return $(`//android.widget.TextView[@text="${name}"]`)
-    }
-}
-
-export default new ProfileScreen();
